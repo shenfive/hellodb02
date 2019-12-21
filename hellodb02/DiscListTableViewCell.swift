6b4eefe2f59247801f09bd3c9d91c150231cb791
Danny Shen <shenfive@gmail.com>
2019-12-21 14:56:18 +0800

    page 2 Cell

---
+++ hellodb02/DiscListTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  DiscListTableViewCell.swift
+//  hellodb02
+//
+//  Created by 申潤五 on 2019/12/21.
+//  Copyright © 2019 申潤五. All rights reserved.
+//
+
+import UIKit
+
+class DiscListTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var title: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- hellodb02/Page2ViewController.swift
+++ hellodb02/Page2ViewController.swift
@@ -8,16 +8,33 @@
 
 import UIKit
 
-class Page2ViewController: UIViewController {
+class Page2ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    
+
+    @IBOutlet weak var tableview: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        tableview.delegate = self
+        tableview.dataSource = self
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(false, animated: false)
+    }
+    
+    
+    //MARK:tableView Delegate
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "discListCell") as! DiscListTableViewCell
+        cell.title.text = "這是我自訂的 Cell"
+        return cell
     }
 }
